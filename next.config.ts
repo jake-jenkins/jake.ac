d47d927a72443598fc9ff1b6c828c27fd2230443
Jake Jenkins <hello@walesdev.com>
2025-02-28 00:46:30 +0000

    add security headers

--- next.config.ts
+++ next.config.ts
@@ -16,8 +16,36 @@ const nextConfig: NextConfig = {
                 source: "/(.*)",
                 headers: [
                     {
+                        key: "Access-Control-Allow-Origin",
+                        value: "https://www.jake.ac"
+                    },
+                    {
+                        key: "Content-Security-Policy",
+                        value: "default-src 'self'; connect-src 'self'; script-src 'strict-dynamic'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data:; font-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; frame-src 'self'; upgrade-insecure-requests;"
+                    },
+                    {
+                        key: "Cross-Origin-Resource-Policy",
+                        value: "same-site"
+                    },
+                    {
+                        key: "Permissions-Policy",
+                        value: "camera=(); geolocation=(); microphone=()"
+                    },
+                    {
+                        key: "Referrer-Policy",
+                        value: "strict-origin-when-cross-origin"
+                    },
+                    {
                         key: "X-Frame-Options",
-                        value: "SAMEORIGIN"
+                        value: "DENY"
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff"
+                    },
+                    {
+                        key: "X-XSS-Protection",
+                        value: "1; mode=block"
                     }
                 ]
             }
